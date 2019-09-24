df2944a7b0af30ee0110048ddbec61ced194218c
Keruila <591055103@qq.com>
2019-09-24 11:38:50 +0800

    完成轮播图数据接口

--- app/views/index_api.py
+++ app/views/index_api.py
@@ -7,12 +7,23 @@ index = Blueprint("index", __name__)
 
 @index.route("/rotation/", methods=["GET"])
 def rotation_chart():
-    result = {
-        "code": 200,
-        "data": [
-            {"img": "/static/img/rotation/HA-1001.png"},
-            {"img": "/static/img/rotation/HA-1039.png"},
-            {"img": "/static/img/rotation/HA-1103.png"},
-        ]
-    }
-    return jsonify(result)
+    rotation_path = "app/static/img/rotation/"
+    img_list = os.listdir(rotation_path)
+    if img_list:
+        data = []
+        for img in img_list:
+            img_path = os.path.join("/static/img/rotation/", img)
+            data.append({"img": img_path})
+        result = {
+            "code": 200,
+            "msg": "请求成功",
+            "data": data
+        }
+        return jsonify(result)
+    else:
+        result = {
+            "code": 204,
+            "msg": "没有数据可返回",
+            "data": []
+        }
+        return jsonify(result)
