f984f76f60c1131dd1925e9ff8fd3d201a0be874
Keruila <591055103@qq.com>
2019-09-24 15:45:11 +0800

    完成家装案例数据接口

--- app/views/index_api.py
+++ app/views/index_api.py
@@ -27,3 +27,27 @@ def rotation_chart():
             "data": []
         }
         return jsonify(result)
+
+
+@index.route("/case/", methods=["GET"])
+def decoration_case():
+    rotation_path = "app/static/img/case/"
+    img_list = os.listdir(rotation_path)
+    if img_list:
+        data = []
+        for img in img_list:
+            img_path = os.path.join("/static/img/case/", img)
+            data.append({"img": img_path})
+        result = {
+            "code": 200,
+            "msg": "请求成功",
+            "data": data
+        }
+        return jsonify(result)
+    else:
+        result = {
+            "code": 204,
+            "msg": "没有数据可返回",
+            "data": []
+        }
+        return jsonify(result)
