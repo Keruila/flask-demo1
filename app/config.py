2eee4cf1f830a23422eff00ab43e3ff1f04552b6
18336908655 <1301792815@qq.com>
2019-09-25 14:35:03 +0800

    修改config

--- app/config.py
+++ app/config.py
@@ -1,4 +1,5 @@
 import os
+import redis
 
 base_dir = os.path.dirname(__file__)
 print(base_dir)
@@ -6,20 +7,31 @@ print(base_dir)
 
 class Config:
     SQLALCHEMY_COMMIT_ON_TEARDOWN = True
+    # redis配置
+    REDIS_HOST = "127.0.0.1"
+    REDIS_PORT = 6379
+
+    # session配置
+    SECRET_KEY = "iHome"
+    # 将session存储到redis中
+    SESSION_TYPE = "redis"
+    SESSION_USE_SIGNER = True
+    SESSION_REDIS = redis.StrictRedis(host=REDIS_HOST, port=REDIS_PORT)
+    PERMANENT_SESSION_LIFETIME = 60 * 60 * 24 * 14  # 秒
 
 
 # 开发环境
-class DevelopConfig:
+class DevelopConfig(Config):
     SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(base_dir, "inter-dev.sqlite")
 
 
 # 测试环境
-class TestConfig:
+class TestConfig(Config):
     SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(base_dir, "inter-test.sqlite")
 
 
 # 生产环境的配置
-class ProductConfig:
+class ProductConfig(Config):
     SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(base_dir, "inter.sqlite")
 
 
