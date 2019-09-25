db9d7c625c6b6fe6f130a30d14cbf413c81cd18f
liudacan <285548136@qq.com>
2019-09-25 21:23:54 +0800

    增加文章接口 删除文章接口 增加评论接口 删除评论接口 查询评论接口

--- app/views/article_api.py
+++ app/views/article_api.py
@@ -1,10 +1,11 @@
 from ..models.user import Article,Comment
+from flask import Blueprint, jsonify, request, session
 from ..extensions import db
 from flask import Blueprint, jsonify, session, request
 
 article = Blueprint("article", __name__, url_prefix="/article")
 
-
+# 增加文章
 @article.route('/add_article/', methods=["POST"])
 def add_article():
     """新增一篇文章"""
@@ -12,20 +13,20 @@ def add_article():
     title = request.form["title"]
     user_id = session.get("user_id", "null")
     if user_id != "null":
-        sav = Article(content=content, title=title, user_id=user_id)
-        db.session.add(sav)
+        save = Article(content=content, title=title, user_id=user_id)
+        db.session.add(save)
         db.session.commit()
         return jsonify({"code": 200, "user_id": user_id, "msg": "发表成功"})
     else:
         return jsonify({"code": 201, "msg": "请先登录"})
 
-
+# 删除文章
 @article.route('/delete_article/<int:article_id>/', methods=["POST"])
 def delete_article(article_id):
     """删除id为article_id的文章"""
-    u = Article.query.get(article_id)
-    if u:
-        db.session.delete(u)
+    save = Article.query.get(article_id)
+    if save:
+        db.session.delete(save)
         db.session.commit()
         return jsonify({"code": 200, "msg": "删除成功"})
     return jsonify({"code": 201, "msg": "您还没有发表过文章"})
@@ -60,13 +61,14 @@ def delete_comment(comment_id):
 # 获得id为article_id的文章下的所有评论
 def replace_id_url(tasks):
     return dict(content=tasks.content, comment_time=tasks.comment_time)
+
+
 @article.route('/all_comments/<int:article_id>/', methods=["POST"])
 def get_all_comments(article_id):
-
     save = Comment.query.filter_by(article_id=article_id).all()
-    if len(save)==0:
-        return jsonify({"code":201, "msg":  "没有找到您需要的内容"})
+    if len(save) == 0:
+        return jsonify({"code": 201, "msg": "没有找到您需要的内容"})
     else:
-        return jsonify({"code": 200, "msg": "获取成功", "save": list(map(replace_id_url, save))})
+        return jsonify({"code": 200, "msg": "获取成功", "data": list(map(replace_id_url, save))})
 
 
