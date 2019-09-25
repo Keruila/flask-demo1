db6e2792487de623bab611945f37a1ac5505d9b6
Keruila <591055103@qq.com>
2019-09-25 10:34:44 +0800

    在manage.py中加跨域

--- manage.py
+++ manage.py
@@ -3,8 +3,10 @@ from app import create_app
 
 from flask_script import Manager
 from flask_migrate import MigrateCommand
+from flask_cors import CORS
 
 app = create_app("default")
+CORS(app, supports_credentials=True)
 manager = Manager(app)
 
 manager.add_command("db", MigrateCommand)
