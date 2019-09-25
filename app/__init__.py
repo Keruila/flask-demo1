05318a7db34a759228662e27692f624776a218cd
liudacan <285548136@qq.com>
2019-09-25 19:00:31 +0800

    增加评论接口 删除评论接口 查询评论接口

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,7 @@
 from flask import Flask
 from app.config import config
 from app.views.user import auth
+from app.views.article_api import article
 from app.views.index_api import index
 from app.views.door_api import product
 from app.extensions import config_extensions
@@ -30,5 +31,6 @@ def create_app(config_name):
     app.register_blueprint(auth)
     app.register_blueprint(index)
     app.register_blueprint(product)
+    app.register_blueprint(article)
     config_extensions(app)
     return app
--- app/views/article_api.py
+++ app/views/article_api.py
@@ -1,5 +1,7 @@
 from ..models.user import Article
-from flask import Blueprint, jsonify
+from ..models.user import Comment
+from ..extensions import db
+from flask import Blueprint, jsonify, session, request
 
 article = Blueprint("article", __name__, url_prefix="/article")
 
@@ -15,22 +17,43 @@ def delete_article(article_id):
     """删除id为article_id的文章"""
     pass
 
-
-@article.route('/add_comment/<int:article_id>', methods=["POST"])
+# 增加评论
+@article.route('/add_comment/<int:article_id>/', methods=["POST"])
 def add_comment(article_id):
     """在id为article_id的文章下添加一条评论"""
-    pass
+    content = request.form["content"]
+    user_id = session.get("user_id")
+    if user_id:
+        save = Comment(content=content, user_id=user_id, article_id=article_id)
+        db.session.add(save)
+        db.session.commit()
+        return jsonify({"code": 200, "msg": "评论成功"})
+    else:
+        return jsonify({"code": 201, "msg": "请先登录后再来评论"})
 
-
-@article.route('/delete_comment/<int:comment_id>', methods=["POST"])
-def add_comment(comment_id):
+# 删除id为comment_id的评论
+@article.route('/delete_comment/<int:comment_id>/', methods=["POST"])
+def delete_comment(comment_id):
     """删除id为comment_id的评论"""
-    pass
+    save = Comment.query.get(comment_id)
+    if save is None:
+        return jsonify({"code": 201, "msg": "未找到该评论"})
+    else:
+        db.session.delete(save)
+        db.session.commit()
+        return jsonify({"code": 200, "msg": "删除成功"})
 
 
-@article.route('/all_comments/<int:article_id>', methods=["POST"])
+# 获得id为article_id的文章下的所有评论
+def replace_id_url(tasks):
+    return dict(content=tasks.content, comment_time=tasks.comment_time)
+@article.route('/all_comments/<int:article_id>/', methods=["POST"])
 def get_all_comments(article_id):
-    """获得id为article_id的文章下的所有评论"""
-    pass
+
+    save = Comment.query.filter_by(article_id=article_id).all()
+    if save is None:
+        return jsonify({"code": "没有找到您需要的内容"})
+    else:
+        return jsonify({"code": 200, "msg": "获取成功", "save": list(map(replace_id_url, save))})
 
 
