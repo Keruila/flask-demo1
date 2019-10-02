aa22b16bd75ca805871a9d5af87783850fe7ace8
18336908655 <1301792815@qq.com>
2019-10-02 15:40:51 +0800

    购物车商品数量的-1

--- app/views/shopping_api.py
+++ app/views/shopping_api.py
@@ -31,14 +31,43 @@ def add_shopping_cart():
     if u :
         u.number += 1
         db.session.commit()
-        return jsonify({"code": 200, "msg": "收藏成功"})
+        return jsonify({"code": 200, "msg": "加入购物车成功"})
     save = ShoppingCart(user_id=user_id, door_id=door_id)
     db.session.add(save)
     db.session.commit()
-    return jsonify({"code": 200, "msg": "收藏成功"})
+    return jsonify({"code": 200, "msg": "加入购物车成功"})
 
 
-# 在购物中删除一个记录
+# 在购物车中已存在的商品的数量 -1
+@shopping.route("/reduce_shopping_cart/", methods=["POST"])
+def reduce_shopping_cart():
+
+    user_id = request.json["user_id"]
+    door_id = request.json["door_id"]
+    u = ShoppingCart.query.filter_by(user_id=user_id,door_id=door_id).first()
+
+    if not user_id:
+        result = {
+            "code": 204,
+            "msg": "请先登录"
+        }
+        return jsonify(result)
+    if not door_id:
+        result = {
+            "code": 204,
+            "msg": "缺少产品id"
+        }
+        return jsonify(result)
+    if u:
+        if u.number == 1:
+            db.session.commit()
+
+            return jsonify({"code":200, "msg": "数量操作成功"})
+        u.number -= 1
+        db.session.commit()
+        return jsonify({"code": 200, "msg": "数量操作成功"})
+    return jsonify({"code":204, "msg":"数据错误"})
+# 在购物车中删除一个记录
 @shopping.route("/delete_shopping_cart/", methods=["POST"])
 def delete_shopping():
     user_id = request.json["user_id"]
