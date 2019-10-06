8d8d2523290b3e89d77da39f2fb41c89e2bb9b81
18336908655 <1301792815@qq.com>
2019-10-06 14:44:13 +0800

    修改支付

--- app/views/orders_api.py
+++ app/views/orders_api.py
@@ -1,18 +1,20 @@
 from ..models.user import Order
 from ..extensions import db
 from flask import Blueprint, jsonify, request
-from alipay import AliPay
 import os
+from alipay import AliPay
 
-alipay = Blueprint("aliplay", __name__)
+alipay = Blueprint("alipay", __name__)
 
 
 @alipay.route("/orders/payment/", methods=["POST"])
 def order_pay():
     user_id = request.json["user_id"]
-    order_id = request.json["id"]
+    order_id = request.json['order_id']
+    total_price = request.json['total_price']
+    print("用户id", user_id, "order", order_id, total_price)
     order = Order.query.filter_by(id=order_id, user_id=user_id, status=0).first()
-    if not order:
+    if order is None:
         return jsonify({"code": 201, "msg": "订单信息有误"})
     else:
         # 实例化支付应用
@@ -27,11 +29,12 @@ def order_pay():
         # 发起支付请求
         order_string = alipay.api_alipay_trade_page_pay(
             out_trade_no=order_id,  # 订单号，多次请求不能一样
-            total_amount=Order.total_price,  # 支付金额
+            total_amount=total_price,  # 支付金额
             subject="双杰木门",  # 交易主题
             return_url="http://localhost:8080/#/pay_success/",
             notify_url=None
         )
+
         pay_url = "https://openapi.alipaydev.com/gateway.do?" + order_string
         return jsonify({"code": 200, "msg": pay_url})
 
@@ -40,11 +43,6 @@ def order_pay():
 def check_pay():
     order_id = request.json["order_id"]
     order = Order.query.filter_by(id=order_id).first()
-    if not order_id and not order:
-        return jsonify({"code": 201, "msg": "未发现订单信息"})
-    # order = Order.query.filter_by(id=order_id).first()
-    if order.status != 0:
-        return jsonify({"code": 201, "msg": "订单已付款"})
     order.status = 1
     db.session.commit()
-    return jsonify({'code': 200, "msg": "支付成功"})
+    return jsonify({'code': 200})
